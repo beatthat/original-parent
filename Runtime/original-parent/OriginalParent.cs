a909f939a287bb07f50e192f4971aadcec598d4b
Larry Kirschner <larrykirschner@gmail.com>
2018-06-26 09:20:16 -0700

    namespaced

--- Runtime/original-parent/OriginalParent.cs
+++ Runtime/original-parent/OriginalParent.cs
@@ -1,14 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-namespace BeatThat
+namespace BeatThat.OriginalParents
 {
-	/// <summary>
-	/// OriginalParent captures the parent of a GameObject so that it can be restored later.
-	/// 
-	/// Useful for things like a popup that gets transfered to a different parent when it opens
-	/// and should be restored to its original parent at the end of use.
-	/// </summary>
-	public class OriginalParent : MonoBehaviour, IOriginalParent
+    /// <summary>
+    /// OriginalParent captures the parent of a GameObject so that it can be restored later.
+    /// 
+    /// Useful for things like a popup that gets transfered to a different parent when it opens
+    /// and should be restored to its original parent at the end of use.
+    /// </summary>
+    public class OriginalParent : MonoBehaviour, IOriginalParent
 	{
 		public bool m_onDisableRestore = true;
 		public GameObject m_originalParent;
